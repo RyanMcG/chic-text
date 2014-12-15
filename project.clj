fe84cc7ee08fde23823b3f0d85d10cda5a3d5063
Ryan McGowan <ryan@ryanmcg.com>
2014-12-14 21:26:21 -0800

    Fill out project.clj and use dev and test profiles.

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
 (defproject chic-text "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Helpful functions for formatting text into tables."
+  :url "https://github.com/RyanMcG/chic-text"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fixturex "0.2.1"]
-                 [jline "2.12"]])
+                 [jline "2.12"]]
+  :profiles {:dev {:plugins [[lein-repack "0.2.5"]]}
+             :test {:dependencies [[fixturex "0.2.1"]]}}
+  :repack [{:type :clojure
+            :levels 1
+            :path "src"
+            :standalone #{"core"}}])
