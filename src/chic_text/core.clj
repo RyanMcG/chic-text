2304e8a10d2b6bfb9b59331b764987c556385d5f
Ryan McGowan <ryan@ryanmcg.com>
2014-12-14 21:18:14 -0800

    Clean up and make some minor fixes to core.

--- src/chic_text/core.clj
+++ src/chic_text/core.clj
@@ -1,50 +1,54 @@
 (ns chic-text.core
-  (:require [clojure.string :as s])
-  (:import [jline TerminalFactory]))
+  (:require [clojure.string :as s]))
 
-(defn max-count [coll] (apply max (map count coll)))
-
+;; Protocols, records and constructors
 (defprotocol ICell
-  (lines [this] "Return a sequential collection of lines.")
-  (line-count [this] "Return the number of lines in a cell."))
+  (lines [this] "Return a sequential collection of lines."))
 
 (defprotocol IRow
-  (cells [this] "Return a sequential collection of cells that make up the row.")
-  (cell-count [this] "Return the number of cells in a row."))
+  (cells [this] "Return a sequential collection of cells that make up the row."))
+
+(defprotocol ILength
+  (length [this] "Return the number of elements in the container."))
 
 (defrecord Cell [contents]
   ICell
   (lines [this] (:contents this))
-  (line-count [this] (count (lines this))))
+  ILength
+  (length [this] (count (lines this))))
 
 (defrecord Row [contents]
   IRow
   (cells [this] (:contents this))
-  (cell-count [this] (count (cells this))))
+  ILength
+  (length [this] (count (cells this))))
+
+(def ^:private will-satisfy? #(partial satisfies? %))
+(def ^:private ilength? (will-satisfy? ILength))
+(def cell? (every-pred (will-satisfy? ICell) ilength?))
+(def row? (every-pred (will-satisfy? IRow) ilength?))
 
 (defn make-cell [lines]
-  {:pre [(every? string? lines)]}
-  (Cell. (vec lines)))
+  {:post [(cell? %)]}
+  (->Cell (vec lines)))
 
-(defn make-row [cells] (Row. (vec cells)))
+(defn make-row [cells]
+  {:pre [(every? cell? cells)]
+   :post [(row? %)]}
+  (->Row (vec cells)))
 
+;; Padding functions
 (defn spaces [n] (apply str (repeat n \space)))
-(defn spaces-cell [space-count] (make-cell [(spaces space-count)]))
-(defn get-terminal-width [] (.getWidth (TerminalFactory/create)))
 
-(def empty-line "")
-(def empty-cell (make-cell [empty-line]))
-
-(defn empty-of [make empty-thing]
-  (fn make-n-empty [n] (make (repeat n empty-thing))))
-
-(def make-empty-cell (empty-of make-cell empty-line))
-(def make-empty-row (empty-of make-row empty-cell))
-
-(defn pad [padding a-str]
+(defn- really-pad [padding a-str]
   {:pre [(>= padding (count a-str))]
    :post [(= padding (count %))]}
   (str a-str (spaces (- padding (count a-str)))))
+
+(defn pad [padding a-str]
+  (if (> (count a-str) padding)
+    a-str
+    (really-pad padding a-str)))
 
 (defn- wrap-line-to-width
   "Turn a string into cell where string lengths are less than or equal to the
@@ -66,31 +70,35 @@
                    (conj! lines line))))
         (persistent! (conj! lines line))))))
 
-(defn make-cell-with-width [width content]
+(defn cell-with-width [width cell]
   (make-cell
-    (->> content
+    (->> cell
          lines
+         (map str)
          (mapcat #(s/split % #"\n"))
          (map (partial wrap-line-to-width width))
          (apply concat)
          (map (partial pad width)))))
 
-(defn- max-counter [select count]
-  (fn a-max-count [coll]
-    (->> coll select (map count) (apply max))))
-
-(defn- extend-with [make count contents padder]
+(defn- extend-with [make contents padder]
   (fn pad-up-to [pad-to-n]
     (fn extend-coll [extendable]
-      (assert (>= pad-to-n (count extendable))
+      (assert (>= pad-to-n (length extendable))
               "padding count must be greater than or equal to the extendable's count")
       (make (concat (contents extendable)
-                    (contents (padder (- pad-to-n (count extendable)))))))))
+                    (contents (padder (- pad-to-n (length extendable)))))))))
 
-(def ^:private extend-cell   (extend-with make-cell line-count lines
-                                          make-empty-cell))
-(def ^:private extend-column (extend-with make-row cell-count cells
-                                          make-empty-row))
+
+(def ^:private empty-line "")
+(def ^:private empty-cell (make-cell [empty-line]))
+
+(defn- empty-of [make empty-thing]
+  (fn make-n-empty [n] (make (repeat n empty-thing))))
+
+(def ^:private make-empty-cell (empty-of make-cell empty-line))
+(def ^:private make-empty-row (empty-of make-row empty-cell))
+(def ^:private extend-cell   (extend-with make-cell lines make-empty-cell))
+(def ^:private extend-column (extend-with make-row cells make-empty-row))
 
 (defn- mix [sources]
   (case (count sources)
@@ -103,10 +111,14 @@
        mix
        (partition n)))
 
-(def max-cell-string-length (max-counter lines count))
-(def max-row-line-count (max-counter cells line-count))
+(defn- max-counter [select count]
+  (fn a-max-count [coll]
+    (->> coll select (map count) (apply max))))
 
-(defn pad-cell-lines [cell]
+(def ^:private max-cell-string-length (max-counter lines count))
+(def ^:private max-row-line-count (max-counter cells length))
+
+(defn- pad-cell-lines [cell]
   (->> cell
        lines
        (map (partial pad (max-cell-string-length cell)))
@@ -119,54 +131,63 @@
        (map (extend-cell (max-row-line-count row)))
        (map pad-cell-lines)
        (map lines)
-       (transpose (cell-count row))
+       (transpose (length row))
        (map (partial apply str))
        (map s/trimr)
        (s/join "\n")))
 
-(def things [{:name "hjjjjjjjjj"
-              :description "kasdjkas dkj sa"}
-             {:name "batman"
-              :description "Hye this is nto a v jdkf dkfjfkd jf sdkjf skj fjkdsjkfjfjsk fjk sdkf sdk fks f sdkf jsdfksjdf ksjd jfd ksf js fk sdj"}])
-
 (defn- normalize-column-to-width [width column]
   (->> column
        cells
-       (map (partial make-cell-with-width width))))
+       (map (partial cell-with-width width))))
+
+(defn- max-count [coll] (apply max (map count coll)))
+
+(defn- columns->rows-of-cells [columns]
+  {:pre [(sequential? columns)
+         (every? sequential? columns)]}
+  (->> columns
+       (map (partial map (comp make-cell vector)))
+       (map make-row)
+       (map (extend-column (max-count columns)))))
 
 (defn widths+columns->str [widths columns]
-  {:pre [(seq widths)
+  {:pre [(sequential? widths)
+         (sequential? columns)
          (every? integer? widths)
-         (seq columns)
-         (every? (partial every? string?) columns)
          (= (count widths) (count columns))]}
-  (->> [widths (->> columns
-                    (map (partial map (comp make-cell vector)))
-                    (map make-row)
-                    (map (extend-column (max-count columns))))]
-       (transpose 2)
-       (map (partial apply normalize-column-to-width))
-       (transpose (count columns))
-       (map make-row)
-       (map row->str)
-       (s/join "\n")))
+  (if (or (empty? columns) (some empty? columns))
+    ""
+    (->> [widths (columns->rows-of-cells columns)]
+         (transpose 2)
+         (map (partial apply normalize-column-to-width))
+         (transpose (count columns))
+         (map make-row)
+         (map row->str)
+         (s/join "\n"))))
 
-(defn column-width [column]
-  (->> column
-       (mapcat #(s/split % #"\n"))
-       (max-count)))
+(defn- column-width [column]
+  (if (empty? column)
+    0
+    (->> column
+         (map str)
+         (mapcat #(s/split % #"\n"))
+         (max-count))))
 
 (defn- column-widths [total-width columns]
   {:pre [(integer? total-width)
-         (seq columns)
-         (every? (partial every? string?) columns)]}
-  (let [columns (butlast columns)
-        widths (vec (map column-width columns))
-        last-width (- total-width (apply + widths))]
-    (assert (>= last-width 0)
-            (str "total width of columns is more than given "
-                 \( total-width \)))
-    (conj widths last-width)))
+         (sequential? columns)
+         (every? sequential? columns)]}
+
+  (if (empty? columns)
+    []
+    (let [columns (butlast columns)
+          widths (vec (map column-width columns))
+          last-width (- total-width (apply + widths))]
+      (assert (>= last-width 0)
+              (str "total width of columns is more than given "
+                   \( total-width \)))
+      (conj widths last-width))))
 
 (defn columns->str [total-width columns]
   (widths+columns->str (column-widths total-width columns)
@@ -180,7 +201,5 @@
       (cond (string? selector) (repeat (count things) (str selector))
             :else (map selector things)))))
 
-(defn terminal-table [& more] (apply table (get-terminal-width) more))
-(defn terminal-table-of [& more] (apply table-of (get-terminal-width) more))
-(def print-table (comp println terminal-table))
-(def print-table-of (comp println terminal-table-of))
+(def print-table (comp println table))
+(def print-table-of (comp println table-of))
