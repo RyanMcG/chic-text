af79c0bfedb83d66b9fc36f2bc726d35d0a08b76
Ryan McGowan <ryan@ryanmcg.com>
2015-02-07 16:53:14 -0800

    Add generated site with incise.

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
-(defproject chic-text/chic-text "0.1.0"
+(defproject chic-text "0.1.0"
   :description "Helpful functions for formatting text into tables."
   :url "https://github.com/RyanMcG/chic-text"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [jline "2.12"]]
-  :profiles {:dev {:plugins [[lein-repack "0.2.5"]]}
-             :test {:dependencies [[fixturex "0.2.1"]]}}
+  :profiles {:dev {:plugins [[lein-repack "0.2.7"]]
+                   :dependencies [[incise "0.5.0"]
+                                  [com.ryanmcg/incise-codox "0.2.0"]
+                                  [com.ryanmcg/incise-vm-layout "0.5.0"]]
+                   :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}}
+             :test {:dependencies [[fixturex "0.3.0"]]}}
   :repack [{:type :clojure
             :levels 1
             :path "src"
